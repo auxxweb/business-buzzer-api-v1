ac48e74a567c4f1d44cd04396fd42cb2a5810408
Karthikk7293 <karthikk7293@gmail.com>
2024-11-21 18:12:50 +0530

    📦 NEW: new google map link added to location object

--- src/modules/business/business.model.ts
+++ src/modules/business/business.model.ts
@@ -31,6 +31,9 @@ const BusinessSchema = new Schema(
       unique: true,
     },
     location: {
+      googleMapLink:{
+        type:String,
+      },
       type: {
         type: String,
         enum: ["Point"],
