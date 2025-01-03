3e17f015e9d225aa7eae6a878872932e85a2ad97
Gurupriyan j <gurupriyanj007@gmail.com>
2025-01-04 00:54:44 +0530

    update the production code

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -3,25 +3,9 @@ import { Request, Response, NextFunction } from "express";
 import { responseUtils } from "../utils/response.utils.js";
 import { errorWrapper } from "../middleware/errorWrapper.js";
 import { successMessages } from "../constants/messages.js";
-import Business from "modules/business/business.model.js";
-
-const createBusId = async (businessCount: number): Promise<string> => {
-  const paddedCount = String(businessCount + 10).padStart(8, "0");
-  return `ENC${paddedCount}`;
-};
 
 const healthCheck = errorWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
-    const businesses = await Business.find({}, { createdAt: 1 });
-    let busCount = 0;
-
-    for (const business of businesses) {
-      // Ensure the businessId is being set correctly
-      business.businessId = await createBusId(busCount); // No need for await if createBusId is not async
-      await business.save(); // Save the updated business
-      busCount += 1; // Increment the counter
-    }
-
     return responseUtils.success(res, {
       data: {
         message: successMessages.healthOk,
