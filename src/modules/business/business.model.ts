7fdddee56f428d96c6ce7fdf0c8fd9ff7673d5c2
auxxweb <155985351+auxxweb@users.noreply.github.com>
2025-10-23 02:19:06 +0530

    undo changes in src

--- src/modules/business/business.model.ts
+++ src/modules/business/business.model.ts
@@ -31,7 +31,7 @@ const BusinessSchema = new Schema(
     businessId: {
       type: String,
       required: true,
-      // unique: true,
+       unique: true,
     },
     location: {
       type: {
